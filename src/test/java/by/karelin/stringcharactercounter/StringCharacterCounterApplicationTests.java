753005942e5f9a2af9752b6fc2f664cd0a673888
Vlad <karelinvlad.dev@gmail.com>
2023-11-26 19:41:55 +0300

    init application

---
+++ src/test/java/by/karelin/stringcharactercounter/StringCharacterCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package by.karelin.stringcharactercounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCharacterCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
