b53aa01798cd9182fb748503452c2248b5ddcb26
Vlad <karelinvlad.dev@gmail.com>
2023-11-26 19:41:55 +0300

    init application

---
+++ src/main/java/by/karelin/stringcharactercounter/StringCharacterCounterApplication.java
@@ -0,0 +1,13 @@
+package by.karelin.stringcharactercounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCharacterCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCharacterCounterApplication.class, args);
+	}
+
+}
